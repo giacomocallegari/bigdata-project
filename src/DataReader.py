a2ebd896200eb73c7342b33e27b6c799b9903cb3
giulitu85 <giuliano.turri@gmail.com>
2019-01-03 22:37:01 +0100

    Create script to read, filter and dispatch csv files

---
+++ src/DataReader.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+import sys
+import csv
+import os
+from TaxiType import TaxiType
+
+
+class DataReader:
+
+    is_a_folder = False
+    type = TaxiType.ALL
+    yellow_set = set()
+    green_set = set()
+    fhv_set = set()
+    yellow_template_header = []
+    fhv_template_header = []
+    green_template_header = []
+
+    def __is_yellow(self, header):
+        if len(header) == len(self.yellow_template_header):
+            for i in range(len(header)):
+                if header[i] != self.yellow_template_header[i]:
+                    return False
+            return True
+
+    def __is_fhv(self, header):
+        if len(header) == len(self.fhv_template_header):
+            for i in range(len(header)):
+                if header[i] != self.fhv_template_header[i]:
+                    return False
+            return True
+
+    def __is_green(self, header):
+        if len(header) == len(self.green_template_header):
+            for i in range(len(header)):
+                if header[i] != self.green_template_header[i]:
+                    return False
+            return True
+
+    def __set_type_param(self, type_param):
+        if type_param == "--yellow" or type_param == "-y":
+            self.type = TaxiType.YELLOW
+        elif type_param == "--green" or type_param == "-g":
+            self.type = TaxiType.GREEN
+        elif type_param == "--FHV" or type_param == "-f":
+            self.type = TaxiType.FHV
+        elif type_param == "--all" or type_param == "-a":
+            self.type = TaxiType.ALL
+        else:
+            print("Invalid Type Parameter")
+            sys.exit()
+
+    def __filter_csv(self, folder_path):
+        try:
+            exist_valid_csv = False
+            for file in os.listdir(folder_path):
+                try:
+                    with open(folder_path + file) as current_file:
+                        reader = csv.reader(current_file)
+                        current_header = next(reader)
+                        if self.__is_yellow(current_header):
+                            exist_valid_csv = True
+                            self.yellow_set.add(folder_path + file)
+                            exist_valid_csv = True
+                        elif self.__is_fhv(current_header):
+                            self.fhv_set.add(folder_path + file)
+                            exist_valid_csv = True
+                        elif self.__is_green(current_header):
+                            exist_valid_csv = True
+                            self.green_set.add(folder_path + file)
+                except Exception:
+                    pass
+            if not exist_valid_csv:
+                print("The selected folder does not contain any csv file")
+                sys.exit()
+        except FileNotFoundError:
+            print("No such file or directory for the path specified")
+            sys.exit()
+
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), "csv-template/yellow.csv")) as yellow_template:
+            reader = csv.reader(yellow_template)
+            self.yellow_template_header = next(reader)
+        with open(os.path.join(os.path.dirname(__file__), "csv-template/fhv.csv")) as fhv_template:
+            reader = csv.reader(fhv_template)
+            self.fhv_template_header = next(reader)
+        with open(os.path.join(os.path.dirname(__file__), "csv-template/green.csv")) as green_template:
+            reader = csv.reader(green_template)
+            self.green_template_header = next(reader)
+        try:
+            if sys.argv[1] == "-r" and (len(sys.argv) == 3 or len(sys.argv) == 4):
+                self.isAFolder = True
+                try:
+                    folder_path = sys.argv[2]
+                    if not folder_path.endswith("/"):
+                        folder_path += "/"
+                    self.__filter_csv(folder_path)
+                    if len(sys.argv) == 4:
+                        self.__set_type_param(sys.argv[3])
+                except IndexError:
+                    print("No folder path specified")
+                    sys.exit()
+            elif len(sys.argv) == 2 or len(sys.argv) == 3:
+                self.is_a_folder = False
+                input_path = sys.argv[1]
+                try:
+                    with open(input_path) as current_file:
+                        reader = csv.reader(current_file)
+                        header = next(reader)
+                        if self.__is_yellow(header):
+                            self.yellow_set.add(input_path)
+                        elif self.__is_fhv(header):
+                            self.fhv_set.add(input_path)
+                        elif self.__is_green(header):
+                            self.green_set.add(input_path)
+                        else:
+                            print("Invalid file selected")
+                            sys.exit()
+                        if len(sys.argv) == 3:
+                            self.__set_type_param(sys.argv[2])
+                except Exception:
+                    print("Invalid file selected")
+                    sys.exit()
+            else:
+                print("Invalid parameters")
+                sys.exit()
+
+        except IndexError:
+            print("You have to specify a folder/file directory")
+            sys.exit()
---
+++ src/TaxiType.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class TaxiType(Enum):
+    ALL = 0
+    YELLOW = 1
+    GREEN = 2
+    FHV = 3
