7223036f4517e36a59f325616cdeba2f05f6b19f
giacomocallegari <giacomo.callegari@studenti.unitn.it>
2019-01-21 16:31:50 +0100

    Make the analysis script support different types of datasets by using parametric fields in queries

--- src/tips-analysis.py
+++ src/tips-analysis.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import hour, dayofyear, month
 
+from TaxiType import TaxiType
 import config
 
 spark = SparkSession.builder\
@@ -10,29 +11,50 @@ spark = SparkSession.builder\
 
 path = config.path  # Specify the path of the data
 filename = 'yellow_tripdata_2018-04.csv'  # Specify the name of the file
+tt = TaxiType.YELLOW  # DEBUG: Type of taxi
+
+# Initialize the correct field names - ATTENTION: field names are represented as the most recent version
+if tt == TaxiType.YELLOW:
+    pu_loc = 'PULocationID'
+    do_loc = 'DOLocationID'
+    do_time = 'tpep_dropoff_datetime'
+    tip = 'tip_amount'
+elif tt == TaxiType.GREEN:
+    pu_loc = 'PULocationID'
+    do_loc = 'DOLocationID'
+    do_time = 'lpep_dropoff_datetime'
+    tip = 'tip_amount'
+elif tt == TaxiType.FHV:
+    pu_loc = 'PULocationID'
+    do_loc = 'DOLocationID'
+    do_time = 'DropOff_datetime'
+    tip = ''  # Tip field not present
+else:
+    pu_loc = ''
+    do_loc = ''
+    do_time = ''
+    tip = ''
 
 df = spark.read.csv(path + filename, header=True, inferSchema=True)  # Save the data into a DataFrame
 # df.show()  # Show the DataFrame
 
 
-# YELLOW TAXI only
-
 # Tip amount per pick-up location
-tips_per_pu = df.groupBy('PULocationID').avg('tip_amount').orderBy('avg(tip_amount)', ascending=False)
+tips_per_pu = df.groupBy(pu_loc).avg(tip).orderBy('avg(' + tip + ')', ascending=False)
 tips_per_pu.show(10)
 
 # Tip amount per drop-off location
-tips_per_do = df.groupBy('DOLocationID').avg('tip_amount').orderBy('avg(tip_amount)', ascending=False)
+tips_per_do = df.groupBy(do_loc).avg(tip).orderBy('avg(' + tip + ')', ascending=False)
 tips_per_do.show(10)
 
 # Tip amount per drop-off time of the day
-tips_per_time = df.groupBy(hour('tpep_dropoff_datetime').alias('hour_of_day')).avg('tip_amount').orderBy('avg(tip_amount)', ascending=False)
+tips_per_time = df.groupBy(hour(do_time).alias('hour_of_day')).avg(tip).orderBy('avg(' + tip + ')', ascending=False)
 tips_per_time.show(10)
 
 # Tip amount per drop-off day of the year
-tips_per_time = df.groupBy(dayofyear('tpep_dropoff_datetime').alias('day_of_year')).avg('tip_amount').orderBy('avg(tip_amount)', ascending=False)
+tips_per_time = df.groupBy(dayofyear(do_time).alias('day_of_year')).avg(tip).orderBy('avg(' + tip + ')', ascending=False)
 tips_per_time.show(10)
 
 # Tip amount per drop-off month of the year
-tips_per_time = df.groupBy(month('tpep_dropoff_datetime').alias('month_of_year')).avg('tip_amount').orderBy('avg(tip_amount)', ascending=False)
+tips_per_time = df.groupBy(month(do_time).alias('month_of_year')).avg(tip).orderBy('avg(' + tip + ')', ascending=False)
 tips_per_time.show(10)
