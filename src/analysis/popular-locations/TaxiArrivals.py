68c52fceefc7c392a2d74019ad1c96041ade717f
giulitu85 <giuliano.turri@gmail.com>
2019-01-25 14:16:53 +0100

    add arrivals charts

--- src/analysis/popular-locations/TaxiArrivals.py
+++ src/analysis/popular-locations/TaxiArrivals.py
@@ -8,15 +8,15 @@ import os
 import shutil
 
 
-class TaxiDepartures:
+class TaxiArrivals:
     spark = SparkSession \
         .builder \
         .appName("max-trips") \
         .getOrCreate()
 
-    yellow_data_path = os.path.join(os.path.dirname(__file__), "output-data/yellow-departures")
-    green_data_path = os.path.join(os.path.dirname(__file__), "output-data/green-departures")
-    fhv_data_path = os.path.join(os.path.dirname(__file__), "output-data/fhv-departures")
+    yellow_data_path = os.path.join(os.path.dirname(__file__), "output-data/yellow-arrivals")
+    green_data_path = os.path.join(os.path.dirname(__file__), "output-data/green-arrivals")
+    fhv_data_path = os.path.join(os.path.dirname(__file__), "output-data/fhv-arrivals")
     shp_file = os.path.join(os.path.dirname(__file__), "../../shapefile/taxi_zones.shp")
     dbf_file = os.path.join(os.path.dirname(__file__), "../../shapefile/taxi_zones.dbf")
 
@@ -30,19 +30,18 @@ class TaxiDepartures:
     def create_dataframe(self, csv_set: list) -> DataFrame:
         return self.spark.read.format("csv").option("header", "true").load(csv_set)
 
-    def get_departures_by_area(self, taxi_df: DataFrame) -> DataFrame:
-        return taxi_df.groupBy("PULocationID").count().\
-            select("PULocationID", col("count").alias("Departures")).where(col("PULocationID") != "")
+    def get_arrivals_by_area(self, taxi_df: DataFrame) -> DataFrame:
+        return taxi_df.groupBy("DOLocationID").count().\
+            select("DOLocationID", col("count").alias("Arrivals")).where((col("DOLocationID") != "") & (col("DOLocationID") < 264))
 
-    def max_departures(self, departures_by_area_df: DataFrame):
-        a = departures_by_area_df.agg({"Departures": "max"}).collect()[0]
-        return a["max(Departures)"]
+    def max_arrivals(self, arrivals_by_area_df: DataFrame):
+        a = arrivals_by_area_df.agg({"Arrivals": "max"}).collect()[0]
+        print(a["max(Arrivals)"])
+        return a["max(Arrivals)"]
 
-
-    def min_departures(self, departures_by_area_df: DataFrame):
-        a = departures_by_area_df.agg({"Departures": "min"}).collect()[0]
-        return a["min(Departures)"]
-
+    def min_arrivals(self, arrivals_by_area_df: DataFrame):
+        a = arrivals_by_area_df.agg({"Arrivals": "min"}).collect()[0]
+        return a["min(Arrivals)"]
 
     def show_charts(self):
         # Creation of chart cuncurrently
@@ -110,53 +109,53 @@ class TaxiDepartures:
     def compute_yellow(self):
         print("Processing yellow taxi,..")
         yellow_df = self.create_dataframe(self.reader.yellow_set)
-        departures_by_area = self.get_departures_by_area(yellow_df)
-        max_dep = self.max_departures(departures_by_area)
+        arrivals_by_area = self.get_arrivals_by_area(yellow_df)
+        max_arr = self.max_arrivals(arrivals_by_area)
         if os.path.isdir(self.yellow_data_path):
             shutil.rmtree(self.yellow_data_path)
-        departures_by_area.write.csv(self.yellow_data_path, header=False)
-        self.yellow_chart = MapChart.MapChart(self.dbf_file, self.shp_file, self.yellow_data_path, 0, max_dep, "Yellow Taxi Departures")
+        arrivals_by_area.write.csv(self.yellow_data_path, header=False)
+        self.yellow_chart = MapChart.MapChart(self.dbf_file, self.shp_file, self.yellow_data_path, 0, max_arr, "Yellow Taxi Arrivals")
 
     def compute_green(self):
         print("Processing green taxi...")
         green_df = self.create_dataframe(self.reader.green_set)
-        departures_by_area = self.get_departures_by_area(green_df)
-        max_dep = self.max_departures(departures_by_area)
+        arrivals_by_area = self.get_arrivals_by_area(green_df)
+        max_arr = self.max_arrivals(arrivals_by_area)
         if os.path.isdir(self.green_data_path):
             shutil.rmtree(self.green_data_path)
-        departures_by_area.write.csv(self.green_data_path, header=False)
-        self.green_chart = MapChart.MapChart(self.dbf_file, self.shp_file, self.green_data_path, 0, max_dep, "Green Taxi Departures")
+        arrivals_by_area.write.csv(self.green_data_path, header=False)
+        self.green_chart = MapChart.MapChart(self.dbf_file, self.shp_file, self.green_data_path, 0, max_arr, "Green Taxi Arrivals")
 
     def compute_fhv(self):
         print("Processing fhv taxi...")
         fhv_df = self.create_dataframe(self.reader.fhv_set)
-        departures_by_area = self.get_departures_by_area(fhv_df)
-        max_dep = self.max_departures(departures_by_area)
+        arrivals_by_area = self.get_arrivals_by_area(fhv_df)
+        max_arr = self.max_arrivals(arrivals_by_area)
         if os.path.isdir(self.fhv_data_path):
             shutil.rmtree(self.fhv_data_path)
-        departures_by_area.write.csv(self.fhv_data_path, header=False)
-        self.fhv_chart = MapChart.MapChart(self.dbf_file, self.shp_file, self.fhv_data_path, 0, max_dep, "Fhv Taxi Departurews")
+        arrivals_by_area.write.csv(self.fhv_data_path, header=False)
+        self.fhv_chart = MapChart.MapChart(self.dbf_file, self.shp_file, self.fhv_data_path, 0, max_arr, "Fhv Taxi Arrivals")
 
 
 reader = DataReader.DataReader()
 reader.read_input_params()
-da = TaxiDepartures(reader)
+aa = TaxiArrivals(reader)
 Taxi_type = TaxiType.TaxiType
 
-if da.reader.type == Taxi_type.ALL:
-    if len(da.reader.yellow_set) != 0:
-        da.compute_yellow()
-    if len(da.reader.green_set) != 0:
-        da.compute_green()
+if aa.reader.type == Taxi_type.ALL:
+    if len(aa.reader.yellow_set) != 0:
+        aa.compute_yellow()
+    if len(aa.reader.green_set) != 0:
+        aa.compute_green()
     if len(reader.fhv_set) != 0:
-        da.compute_fhv()
-    da.show_charts()
+        aa.compute_fhv()
+    aa.show_charts()
 elif reader.type == Taxi_type.YELLOW:
-    da.compute_yellow()
-    da.yellow_chart.create_chart()
+    aa.compute_yellow()
+    aa.yellow_chart.create_chart()
 elif reader.type == Taxi_type.FHV:
-    da.compute_fhv()
-    da.fhv_chart.create_chart()
+    aa.compute_fhv()
+    aa.fhv_chart.create_chart()
 elif reader.type == Taxi_type.GREEN:
-    da.compute_green()
-    da.green_chart.create_chart()
+    aa.compute_green()
+    aa.green_chart.create_chart()
